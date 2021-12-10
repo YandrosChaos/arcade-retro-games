69480de85782a8c1fbedff5db97bc846766fc92a
SnowCrash <victor.develops@gmail.com>
2021-12-10 12:47:43 +0100

    show user data in slider menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { App } from "@capacitor/app";
 import { Platform } from "@ionic/angular";
+import { Subscription } from "rxjs";
+import { User } from "./commons/interfaces/user/user.class";
+import { HolyData } from "./commons/services/holy-data/holy-data.service";
+import { UserService } from "./commons/services/user/user.service";
 
 @Component({
   selector: "app-root",
   templateUrl: "app.component.html",
   styleUrls: ["app.component.scss"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  private subUser: Subscription;
+  public user: User = new User();
+
   constructor(private platform: Platform, private router: Router) {
     this.platform.backButton.subscribeWithPriority(0, () => {
       this.router.navigate(["/home"]);
@@ -16,5 +23,15 @@ export class AppComponent {
     this.platform.backButton.subscribeWithPriority(-1, () => {
       App.exitApp();
     });
+  }
+
+  ngOnInit(): void {
+    this.subUser = UserService.getCurrent().subscribe((user) =>
+      this.user.assign(user)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subUser.unsubscribe();
   }
 }
