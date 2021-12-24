83e38790722bb1a716685a42fcd656c8e6725f5d
SnowCrash <victor.develops@gmail.com>
2021-12-24 15:20:27 +0100

    score.scene touch sound and tap counter

--- src/app/game-loader/scenes/score.scene.ts
+++ src/app/game-loader/scenes/score.scene.ts
@@ -2,18 +2,25 @@ import Phaser from "phaser";
 import { calculateHalfOfHalf } from "src/app/commons/functions/responsive.function";
 import { UserService } from "src/app/commons/services/user/user.service";
 import { Scene } from "../game-objects/scene";
+import { SOUND_EFFECTS_VOLUME } from "./k-boom.config";
 import {
   BACKGROUND_IMG_PATH,
   BACKGROUND_SECTION_NAME,
   SCORE_SCENE_NAME,
+  START_SOUND_PATH,
+  START_SOUND_SECTION_NAME,
   WELCOME_SCENE_NAME,
 } from "./k-boom.routes";
 
 export class ScoreScene extends Scene {
   private score: number;
+  private tapCounter: number;
+
   private result: Phaser.GameObjects.Text;
   private hint: Phaser.GameObjects.Text;
   private hintText: string = "Touch to restart";
+
+  private touchSound: Phaser.Sound.BaseSound;
 
   constructor() {
     super(SCORE_SCENE_NAME);
@@ -26,6 +33,9 @@ export class ScoreScene extends Scene {
 
   preload() {
     this.load.image(BACKGROUND_SECTION_NAME, BACKGROUND_IMG_PATH);
+    this.load.audio(START_SOUND_SECTION_NAME, START_SOUND_PATH);
+
+    this.tapCounter = 0;
   }
 
   update(time: number): void {
@@ -46,6 +56,7 @@ export class ScoreScene extends Scene {
 
   create(): void {
     super.fadeInScene();
+    this.buildSounds();
     const background: Phaser.GameObjects.TileSprite = this.add.tileSprite(
       0,
       this.renderer.height / 2,
@@ -67,9 +78,24 @@ export class ScoreScene extends Scene {
     this.input.on(
       "pointerdown",
       () => {
-        super.fadeOutScene(WELCOME_SCENE_NAME);
+        this.closeScene();
       },
       this
     );
+  }
+
+  private closeScene(): void {
+    if (this.tapCounter >= 1) {
+      this.touchSound.play();
+      super.fadeOutScene(WELCOME_SCENE_NAME);
+    } else {
+      this.tapCounter++;
+    }
+  }
+
+  private buildSounds(): void {
+    this.touchSound = this.sound.add(START_SOUND_SECTION_NAME, {
+      volume: SOUND_EFFECTS_VOLUME,
+    });
   }
 }
